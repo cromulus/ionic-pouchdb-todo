3bad33bcfba6df888c42e48f90771f1c407abb75
Bill Cromie <bill@cromie.org>
2014-08-14 18:24:13 -0400

    pouch updater is now more considerate of couchdb's concerns

--- pouch_updater.js
+++ pouch_updater.js
@@ -9,6 +9,27 @@ var newbfile = __dirname + '/newbs.json';
 var newb_data;
 var mentor_data;
 
+
+function intersect_safe(a, b)
+{
+  var ai=0, bi=0;
+  var result = new Array();
+
+  while( ai < a.length && bi < b.length )
+  {
+     if      (a[ai]["_id"] < b[bi]["_id"] ){ ai++; }
+     else if (a[ai]["_id"] > b[bi]["_id"] ){ bi++; }
+     else /* they're equal */
+     {
+       result.push(a[ai]);
+       ai++;
+       bi++;
+     }
+  }
+
+  return result;
+}
+
 mentorSync = mentorDb.sync('https://pouchdb:pouchdbpassword8@gpementor.iriscouch.com/mentors', {live: true})
   .on('error', function (err) {
     console.log("Syncing stopped");
@@ -38,12 +59,19 @@ fs.readFile(mentorfile, 'utf8', function (err, data) {
   }
 
   mentor_data = JSON.parse(data);
-  mentorDb.bulkDocs(mentor_data,function(err, res) {
-    if (err) console.log(err);
-    console.log(res);
+  newbDb.allDocs({include_docs: true},function(err, response){
+    var arr=[];
+    for (var i = 0; i < response.rows.length; i++) {
+      arr.push(response.rows[i].doc)
+    }
+    new_mentors=intersect_safe(arr,mentor_data);
+    console.log("new_mentors");
+    console.log(new_mentors);
+    mentorDb.bulkDocs(new_mentors,function(err, res) {
+      if (err) console.log(err);
+      console.log(res);
+    });
   });
-
-
 });
 
 fs.readFile(newbfile, 'utf8', function (err, data) {
@@ -52,9 +80,19 @@ fs.readFile(newbfile, 'utf8', function (err, data) {
     return;
   }
   newb_data = JSON.parse(data);
-  newbDb.bulkDocs(newb_data,function(err, res) {
-    if (err) console.log(err);
-    console.log(res);
+
+  newbDb.allDocs({include_docs: true},function(err, response){
+    var arr=[];
+    for (var i = 0; i < response.rows.length; i++) {
+      arr.push(response.rows[i].doc)
+    }
+    new_newbs=intersect_safe(arr,newb_data);
+    newbDb.bulkDocs(newb_data,function(err, res) {
+      if (err) console.log(err);
+      console.log(res);
+    });
+    console.log("new_newbs");
+    console.log(new_newbs);
   });
 });
 
